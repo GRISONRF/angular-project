c0dea863ff9e86800a728c5815049f2e3ac836b7
Grison <Rafaela.Grison@cartus.com>
2024-03-26 08:10:52 -0500

    add toastr - not working

--- src/app/events/events-list.component.ts
+++ src/app/events/events-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from "@angular/core"
 import { EventService } from "./shared/event.service";
 
+declare let toastr
 @Component({
     selector: 'events-list',
     template: `
@@ -9,7 +10,7 @@ import { EventService } from "./shared/event.service";
         <hr/>
         <div class="row">
             <div *ngFor="let event of events" class="col-md-5">
-                <event-thumbnail [event]="event"></event-thumbnail>
+                <event-thumbnail (click)="handleThumbnailClick(event.name)" [event]="event"></event-thumbnail>
             </div>
         </div>
         
@@ -23,6 +24,9 @@ export class EventsListComponent implements OnInit {
 
     ngOnInit() {
       this.events = this.eventService.getEvents()
+    }
 
+    handleThumbnailClick(eventName){
+      toastr.success(eventName)
     }
 }
