b2250486b3a58cdbe1c0af4a5b50f4e4e0956a42
Grison <grisonrf@gmail.com>
2024-04-05 14:09:50 -0500

    create login template

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -3,6 +3,8 @@ import { CommonModule } from "@angular/common";
 import { RouterModule } from "@angular/router";
 import { userRoutes } from './user.routes';
 import { ProfileComponent } from "./profile.component";
+import { LoginComponent } from './login.component';
+
 
 @NgModule({
     imports: [
@@ -10,7 +12,8 @@ import { ProfileComponent } from "./profile.component";
         RouterModule.forChild(userRoutes)
     ],
     declarations: [
-        ProfileComponent
+        ProfileComponent,
+        LoginComponent,
     ],
     providers: [
 
